cf6bae3c5e2012301ffa3852187c98a7a1324083
interimadd <interimadd@gmail.com>
2020-02-29 16:15:29 +0900

    feat: Googleアカウントを使ったログインに対応

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Task from '@/components/Task.vue'
 import Signup from '@/components/Signup'
 import Signin from '@/components/Signin'
+import firebase from 'firebase'
  
 Vue.use(Router)
  
@@ -26,6 +27,23 @@ let router =  new Router({
     }
   ]
 })
- 
- 
+
+// ログインが完了していない場合にサインインページの飛ばす
+router.beforeEach((to, from, next)=>{
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  let currentUser = firebase.auth().currentUser
+  if (requiresAuth){
+  if (!currentUser){
+    next({
+      path: '/signin',
+      query: {redirect: to.fullPath}
+    })
+  }else{
+    next()
+  }
+}else{
+  next()
+}
+})
+
 export default router
