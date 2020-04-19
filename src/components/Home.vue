96f4ea16807c04d2a7346c37363259bbd14679c5
interimadd <interimadd@gmail.com>
2020-04-19 18:30:07 +0900

    feat: ランディングページを追加

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,9 +6,9 @@
           <div class="has-text-weight-bold has-text-white is-size-3">同人サークル<span class="is-size-4">のための</span></div>
           <p class="has-text-weight-bold has-text-white is-size-3">クラウド在庫管理システム</p>
           <div class="has-text-white is-size-5">必要最小限のシンプルなWebアプリ</div>
-          <div class="has-text-white is-size-5">無料で使えます</div>
+          <div class="has-text-white is-size-5">ブラウザからインストール不要で使えます</div>
           <div>.</div>
-          <div class="button is-primary is-rounded has-text-weight-bold">使ってみる</div>
+          <a class="button is-primary is-rounded has-text-weight-bold" href="/signin">使ってみる</a>
         </div>
         <div class="column">
           <img src="/pc_phone.png"/>
@@ -16,20 +16,97 @@
         <div class="column is-1"></div>
       </div>
     </div>
-    <div class="columns is-centered">
-      <div class="column is-half is-full-mobile is-offset-1-mobile">
-        <h1 class="has-text-weight-bold is-size-3">在庫管理Appの特徴</h1>
-        <div class="has-text-weight-bold is-size-3">同人サークル<span class="is-size-4">のための</span></div>
-        <p class="has-text-weight-bold is-size-3">クラウド在庫管理システム</p>
-        <div class="is-size-5">必要最小限のシンプルなWebアプリ</div>
-        <div class="is-size-5">無料で使えます</div>
+    <div class="container has-text-centered line_margin">
+        <h1 class="has-text-weight-bold is-size-3">— 在庫管理Appの特徴 —</h1>
+        <br>
+        <div class="columns is-mobile is-centered">
+          <div class="column is-2-desktop">
+            <div class="is-size-4 has-text-weight-bold">簡単在庫管理</div>
+            <img src="/stock_icon.png"/>
+          </div>
+          <div class="column is-2-desktop">
+            <div class="is-size-4 has-text-weight-bold">どこでも使える</div>
+            <img src="/cloud_icon.png"/>
+          </div>
+          <div class="column is-2-desktop">
+            <div class="is-size-4 has-text-weight-bold">完全無料</div>
+            <img src="/free_icon.png"/>
+          </div>
+        </div>
+        <br>
+        <p class="has-text-weight-bold has-text-primary is-size-3">面倒な在庫管理が簡単に！</p>
+        <div class="is-size-5">今までスプレッドシートで管理していた印刷所への発注やイベントでの販売個数等の情報を、簡単な操作でミス無く管理できます</div>
+        <div class="is-size-5">Googleやtwitterの共通アカウントを使って、メンバーの誰でも同じ情報にアクセスできます</div>
+        <br>
+        
+        <div class="is-size-4 has-text-weight-bold">シンプルな入庫/出庫履歴の管理</div>
+        <div class="is-size-5">あらかじめ登録しておいた商品名や場所の情報を参照し、最小限の情報を入力するだけで、入庫/出庫の情報を登録することができます</div>
+        <div class="is-size-5">登録した情報は新しい順番にリストで表示されます</div>
+        <img src="/input_sample.gif"/>
+
+        <div class="is-size-4 has-text-weight-bold">在庫数の確認</div>
+        <div class="is-size-5">入庫/出庫の履歴から在庫がどこにいくつあるのかを自動で集計し、一覧を確認することができます</div>
+        <div class="is-size-5">今までに作ったものがいくつ売れたのかを確認するのにも役立ちます</div>
+        <img src="/count_sample.png"/>
+        <br>
+        
+        <p class="has-text-weight-bold has-text-primary is-size-3">PCでもスマホでも利用できる！</p>
+        <br>
+        <div class="is-size-5">ウェブブラウザを使って外出先からでも簡単にアクセスできます</div>
+        <div class="is-size-5">レスポンシブデザイン対応で、パソコンでもスマートフォンでも同じ機能を使うことができます</div>
+        <img src="/responsive_sample.gif"/>
+        <br>
+
+        <a class="columns is-mobile is-centered" href="/signin">
+          <div class="column is-half">
+            <div class="button is-primary is-rounded is-large is-fullwidth has-text-weight-bold">使ってみる</div>
+          </div>
+        </a>
       </div>
-    </div>
+
+      <br>
+
+      <footer class="footer">
+        <nav class="level">
+          <div class="level-item">
+            <div>
+              <div><strong>製作者</strong> <a href="https://twitter.com/interimadd">@interimadd</a></div>
+              ご意見ご要望は上記のtwitterアカウントまでお願いします。
+            </div>
+          </div>
+          <div class="level-item has-text-centered">
+            <div class="twitter_share">
+              <button class="button is-info"  @click="twitterShare">
+                <span class="icon"><i class="fa fa-lg fa-twitter"></i></span>
+                <span>ツイート</span>
+              </button>
+            </div>
+          </div>
+        </nav>
+      </footer>
   </div>
 </template>
 
+<script>
+export default {
+  methods:{
+    twitterShare(){
+      const tweet_content = "-同人サークルのためのクラウド在庫管理システム- 在庫管理App%0a"
+      let shareURL = 'https://twitter.com/intent/tweet?text=' + tweet_content + '&url=' + "https://azb-stock-management.firebaseapp.com";  
+      location.href = shareURL
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 @import '~bulma';
+$fa-font-path: '~font-awesome/fonts/';
+@import '~font-awesome/scss/font-awesome';
+
+img {
+  padding: 20px;
+}
 
 .section1{
   /*レイアウト用のCSS*/
@@ -52,8 +129,16 @@
  
   /*ここで三角形のサイズを決める。
   必ず幅100vwにして、ブラウザサイズいっぱいにしてください。*/
-  border-width: 0 0 200px 120vw ;
+  border-width: 0 0 100px 120vw ;
  
   border-color: transparent transparent #fff transparent;
+}
+
+.line_margin{
+  line-height: 40px
+}
+
+footer {
+  background-color: $grey-darker;
 }
 </style>
